de1d0159488b5e2b8fc29aea088eebb6fe499c44
daiki2828 <114804118+daiki2828@users.noreply.github.com>
2022-11-01 07:32:46 +0000

    [finish]課題2修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,15 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
-    @user = current_user
+    @book_new = Book.new
   end
 
   def index
-    @book = Book.new
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -22,6 +24,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
     @book = Book.find(params[:id])
   end
 
@@ -44,5 +47,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+      @book = Book.find(params[:id])
+      unless @book.user == current_user
+         redirect_to books_path
+      end
   end
 end
