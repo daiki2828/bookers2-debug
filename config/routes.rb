86ecbbba601b3bbdc01dbb130712c44e2a7a2851
daiki2828 <114804118+daiki2828@users.noreply.github.com>
2022-11-08 07:08:06 +0000

    課題7c

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   end
   
   resources :chats, only: [:show, :create]
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
