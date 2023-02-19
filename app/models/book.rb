f9ed72c503c2dacd432e1768a21377fc63d37133
daiki2828 <114804118+daiki2828@users.noreply.github.com>
2023-02-19 08:50:04 +0000

    コミット

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,6 @@ class Book < ApplicationRecord
   has_many :view_counts, dependent: :destroy
   
   validates :title,presence:true
-  validates :body,presence:true,length:{maximum:200}
 
 
   def favorited_by?(user)
